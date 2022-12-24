3599d1be40808f3b3ed2d974118b7c013279161b
josemlrod <mrjosemlrod@gmail.com>
2022-12-24 00:05:12 -0500

    added placeholder navigation bar

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,56 @@
 export default function Index() {
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
+    <nav
+      aria-label="Site Nav"
+      className="mx-auto flex items-center justify-between p-4 dark:bg-zinc-900"
+    >
+      <a
+        href="/"
+        className="inline-flex h-10 w-10 items-center justify-center rounded-lg bg-gray-100 dark:bg-zinc-800"
+      >
+        <span className="sr-only">Logo</span>
+        👋
+      </a>
+
+      <ul className="flex items-center gap-2 text-sm font-medium text-gray-500 dark:text-white">
+        <li className="hidden lg:block">
+          <a className="rounded-lg px-3 py-2" href="/">
+            {" "}
+            Home{" "}
           </a>
         </li>
+
         <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
+          <a className="rounded-lg px-3 py-2" href="">
+            {" "}
+            Projects{" "}
           </a>
         </li>
+
         <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
+          <a
+            className="inline-flex items-center rounded-lg px-3 py-2"
+            href=""
+            target="_blank"
+          >
+            External
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="ml-1.5 h-4 w-4"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+              ></path>
+            </svg>
           </a>
         </li>
       </ul>
-    </div>
+    </nav>
   );
 }
